06b04b59916f4488c644e1bb2b0b0fb77e8ba453
Thomas Figueroa <splines21@outlook.com>
2022-08-31 13:52:28 -0500

    Fix exports in formatter_pointer.h

--- libcxx/include/__format/formatter_pointer.h
+++ libcxx/include/__format/formatter_pointer.h
@@ -56,10 +56,10 @@ public:
 // - struct formatter<nullptr_t, charT>;
 // - template<> struct formatter<void*, charT>;
 // - template<> struct formatter<const void*, charT>;
-_LIBCPP_EXPORT_STD template <__fmt_char_type _CharT>
+template <__fmt_char_type _CharT>
 struct _LIBCPP_TEMPLATE_VIS _LIBCPP_AVAILABILITY_FORMAT formatter<nullptr_t, _CharT>
     : public __formatter_pointer<_CharT> {};
-_LIBCPP_EXPORT_STD template <__fmt_char_type _CharT>
+template <__fmt_char_type _CharT>
 struct _LIBCPP_TEMPLATE_VIS _LIBCPP_AVAILABILITY_FORMAT formatter<void*, _CharT> : public __formatter_pointer<_CharT> {
 };
 template <__fmt_char_type _CharT>
